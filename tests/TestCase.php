cde2e50ba2f0b6a6ea4177a15aaf27a078c3dfaf
Rommel Galisanao <rommel.galisanao.22@gmail.com>
2026-07-14 00:14:07 +0800

    test: disable Vite during feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
+
     public function actingAs($user, $guard = null)
     {
         parent::actingAs($user, $guard);
